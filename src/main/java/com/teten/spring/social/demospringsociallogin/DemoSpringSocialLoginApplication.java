fda28318152d879f02fc95641a22c8b302829b4c
teten777 <teten.nugraha18@gmail.com>
2019-08-13 10:37:37 +0700

    first

---
+++ src/main/java/com/teten/spring/social/demospringsociallogin/DemoSpringSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.teten.spring.social.demospringsociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSocialLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teten/spring/social/demospringsociallogin/DemoSpringSocialLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teten.spring.social.demospringsociallogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringSocialLoginApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
